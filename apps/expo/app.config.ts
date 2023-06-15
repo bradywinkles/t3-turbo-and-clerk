94f0dc7a88d2834e249bc96b09bc157864cadec4
bradywinkles <brady@cityoftreesrealestate.com>
2023-06-15 10:38:34 -0600

    ts-with-clerk

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_YWN0dWFsLWFsaWVuLTU0LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
